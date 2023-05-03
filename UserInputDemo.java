f74cf169861ac50bc3dd97a5f39a1c9b004347af
RohanAwasthi <128706839+RohanAwasthi@users.noreply.github.com>
2023-05-03 17:36:53 +0530

    Add files via upload

---
+++ UserInputDemo.java
@@ -0,0 +1,26 @@
+package polymoiosphism;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class UserInputDemo {
+    public static void main(String[] args) {
+        
+
+        System.out.println("Enter a number");
+
+        Scanner sc = new Scanner(System.in);
+        int n2 = sc.nextInt();
+        System.out.println(n2);
+        
+
+
+
+
+
+
+    }
+}
+
